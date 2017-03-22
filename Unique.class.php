52c5155b6370cd3e3db9ccf542628ad4f34f38a2
Suker Xu <2312894600@qq.com>
2017-03-22 10:10:37 +0800

    Unique.class.php
    Unique类，用于生成随机值。

---
+++ Unique.class.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Unique类，用于生成随机值
+ *
+ * @desc 产生唯一值
+ * @author guohuixu
+ * @date 2017-03-22
+ */
+class Unique
+{
+    /**
+     * 根据干扰值，产生唯一值
+     *
+     * @author chengjinsheng
+     * @date 2017-02-18
+     *
+     * 返回md5之后的32个字符
+     * @param string $annoyance
+     * @return string
+     */
+    public static function get($annoyance)
+    {
+        $str = $annoyance . microtime(true) . self::randStr();
+        return md5($str);
+    }
+
+    /**
+     * 随机产生字符串
+     *
+     * @author chengjinsheng
+     * @date 2017-02-18
+     *       
+     * @param int $length
+     *
+     * @return string
+     */
+    public static function randStr($length=6) {
+
+        $pool = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        return substr(str_shuffle(str_repeat($pool, $length)), 0, $length);
+    
+    }
+    
+}
+
